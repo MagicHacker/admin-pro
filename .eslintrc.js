df2d26aef35c677782027edf2fcbcace8f68d99f
Johnson <zxmagichacker@gmail.com>
2021-02-13 14:38:37 +0800

    feat: 显示webpack等配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ module.exports = {
   extends: ['alloy', 'alloy/react'],
   env: {
     browser: true,
-    node: true
+    node: true,
+    jest: true
+  },
+  rules: {
+    'max-params': 'off',
+    complexity: 'off'
   }
 }
