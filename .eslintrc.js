8d22adeb8f8ed8327bfd39bd9484a921b676b4eb
Johnson <zxmagichacker@gmail.com>
2021-02-15 16:17:17 +0800

    feat: 引入eslint-plugin-prettier和eslint-config-prettier,并格式化文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: ['alloy', 'alloy/react'],
+  plugins: ['react', 'prettier'],
   env: {
     browser: true,
     node: true,
     jest: true
   },
   rules: {
+    'prettier/prettier': 'error',
     'max-params': 'off',
     complexity: 'off'
   }
-}
+};
