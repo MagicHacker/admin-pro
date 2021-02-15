640dc59519672be0cc7e1f2ecd19251d4d0fd59d
Johnson <zxmagichacker@gmail.com>
2021-02-15 13:06:08 +0800

    feat: 重构App组件

---
+++ src/pages/app/index.js
@@ -0,0 +1,16 @@
+
+import './index.less';
+import { Component } from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render() {
+    return <div className="app-wrapper">我是你爸爸</div>
+  }
+}
+export default App;
