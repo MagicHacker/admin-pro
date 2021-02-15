adb866e7effceb9dc0fc9b116018feed043acee4
Johnson <zxmagichacker@gmail.com>
2021-02-15 23:03:59 +0800

    feat: 更改成路由形式

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -1,20 +1,22 @@
-
 import './index.less';
-import { Component } from 'react'
-import Login from '../login/index'
+import { Component } from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Login from '../login/index';
+import MainPage from '../main-page/index';
 class App extends Component {
   constructor(props) {
-    super(props)
-    this.state = {
-
-    }
+    super(props);
+    this.state = {};
   }
   render() {
     return (
       <div className="app-wrapper">
-        <Login />
+        <Router>
+          <Route path="/login" component={Login} />
+          <Route path="/mainPage" component={MainPage} />
+        </Router>
       </div>
-    )
+    );
   }
 }
 export default App;
