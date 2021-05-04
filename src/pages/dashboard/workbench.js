cb63ef908cb6ea738dfbd776ae5a18333e3437e5
Johnson <zxmagichacker@gmail.com>
2021-05-04 16:59:16 +0800

    feat: 构建工作台

--- src/pages/dashboard/workbench.js
+++ src/pages/dashboard/workbench.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-
+import './workbench.less';
+import { Row, Col, Card } from 'antd';
 export default class Workbench extends Component {
   render() {
     return (
-      <div>
-        <div>22</div>
-      </div>
+      <>
+        <div className="workbench_wrapper">
+          <Row gutter={[20, 20]}>
+            <Col>
+              <Card title="进行中的项目" extra={<a href="#">全部项目</a>}>
+                1
+              </Card>
+              <Card title="动态">2</Card>
+            </Col>
+            <Col>
+              <Card title="快捷开始/快捷导航">3</Card>
+              <Card title="xx指数">4</Card>
+              <Card title="团队">5</Card>
+            </Col>
+          </Row>
+        </div>
+      </>
     );
   }
 }
